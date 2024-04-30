5b23916615cccbd18370d80ace40a030d8f086d7
amitw-19 <tarunwarathe@gmail.com>
2024-04-30 19:15:47 +0530

    updated url

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const URL = "http://localhost:8000";
+const URL =
+  "http://localhost:8000" || "https://flipkart-backend-six.vercel.app/";
 
 export const authenticateSignup = async (data) => {
   try {
