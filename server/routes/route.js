4e3311bc07dbccc9ba32a64ec98eeb810c4f4727
amitw-19 <tarunwarathe@gmail.com>
2024-05-01 20:47:05 +0530

    cors

--- server/routes/route.js
+++ server/routes/route.js
@@ -8,12 +8,12 @@ import {
 
 const router = express.Router();
 
-app.all("*", function (req, res, next) {
+/*app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
-});
+});*/
 
 router.post("/signup", userSignup);
 router.post("/login", userLogin);
