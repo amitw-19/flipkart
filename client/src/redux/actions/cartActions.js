36e68100f042756efa07e28a2a80bbd434685e53
amitw-19 <tarunwarathe@gmail.com>
2024-05-08 22:17:33 +0530

    url

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import * as actionType from "../constants/cartConstant";
 
-const URL = "https://flipkart-backend-six.vercel.app/";
+const URL = "";
 
 export const addToCart = (id, quantity) => async (dispatch) => {
   try {
